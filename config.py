6e6ffa397d58f52f70dcf2d9e50328f59804e725
Aleksei <svid.aleksei@ya.ru>
2025-02-20 20:52:14 +0300

    init project and branch

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+ROOT_DIR = os.path.dirname(__file__)
+# LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+# DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+
+if __name__ == "__main__":
+    print(__file__)
+    print(ROOT_DIR)
+    # print(LOGS_DIR)
+    # print(DATA_DIR)
